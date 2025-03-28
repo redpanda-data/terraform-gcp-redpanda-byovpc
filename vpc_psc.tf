41feb8a74f37d9d047f7b14837f40717f2a5ab40
Sarah Haskins <sarah.haskins@redpanda.com>
2025-03-28 15:07:39 -0400

    Make psc subnet creation optional
    If variable `eanble_private_link` is true the caller can choose
    to provide a pre-existing subnet name in which case creation
    of the subnet will be skipped.

--- vpc_psc.tf
+++ vpc_psc.tf
@@ -1,14 +1,23 @@
+locals {
+  create_psc_subnetwork = local.create_vpc && var.enable_private_link
+}
 
 # This resource isn't required for all BYOVPC deployments.
 # It's only here to make testing Private Link on BYOVPC clusters a little bit easier.
 resource "google_compute_subnetwork" "psc_nat" {
-  count         = var.enable_private_link ? 1 : 0
+  count         = local.create_psc_subnetwork ? 1 : 0
   name          = "redpanda-psc-nat-subnetwork${local.postfix}"
   region        = var.region
   ip_cidr_range = var.psc_nat_subnet_ipv4_range
   purpose       = "PRIVATE_SERVICE_CONNECT"
   network       = data.google_compute_network.redpanda.id
   project       = var.network_project_id
+}
+
+data "google_compute_subnetwork" "psc_nat" {
+  name    = local.create_psc_subnetwork ? google_compute_subnetwork.psc_nat[0].name : var.psc_subnet_name
+  region  = var.region
+  project = var.network_project_id
 }
 
 data "google_iam_policy" "nat_subnet_iam" {
@@ -25,8 +34,8 @@ data "google_iam_policy" "nat_subnet_iam" {
 # Required so the service attachment can use the PSC NAT subnet, which exists in the HOST project.
 resource "google_compute_subnetwork_iam_policy" "nat_subnet_policy" {
   count       = local.is_shared_vpc && var.enable_private_link ? 1 : 0
-  project     = google_compute_subnetwork.psc_nat[0].project
-  region      = google_compute_subnetwork.psc_nat[0].region
-  subnetwork  = google_compute_subnetwork.psc_nat[0].name
+  project     = data.google_compute_subnetwork.psc_nat.project
+  region      = data.google_compute_subnetwork.psc_nat.region
+  subnetwork  = data.google_compute_subnetwork.psc_nat.name
   policy_data = data.google_iam_policy.nat_subnet_iam[0].policy_data
 }
